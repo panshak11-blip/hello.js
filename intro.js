2817d098bf3da538d94bf2636a4299a902fe7e59
Panshak Reuben <reubenpanshak@gmail.com>
2026-09-12 15:43:22 +0100

    Add intro.js

---
+++ intro.js
@@ -0,0 +1,9 @@
+// intro.js
+
+const name = "Panshak";
+const age = 20;
+const favouriteLanguage = "HTML and CSS";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favourite Programming Language: " + favouriteLanguage);
